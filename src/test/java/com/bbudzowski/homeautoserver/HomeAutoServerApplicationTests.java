e53559aaa369a2821d3e7580d8f27f614c461cac
weaweg <budzowski.blazej@gmail.com>
2023-04-24 17:27:03 +0200

    Project init

---
+++ src/test/java/com/bbudzowski/homeautoserver/HomeAutoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbudzowski.homeautoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAutoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
